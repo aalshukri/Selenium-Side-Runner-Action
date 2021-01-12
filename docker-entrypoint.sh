7999b07ad3890b455f2b664db6291e1a2368390e
aalshukri <aalshukri@googlemail.com>
2021-01-12 16:40:30 +0000

    test.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,16 +7,18 @@ mkdir /out
 
 selenium-side-runner -s http://chromedriver:4444 --output-directory /out /sides/*.side
 
-ls -l
 
-
-if [ -z "${INPUT_SIDE-DIRECTORY}" ]; then
+if [ -n "${INPUT_SIDE-DIRECTORY}" ]; then
   echo "::debug::Input side directory provided."
   echo "::debug::Using \"${INPUT_SIDE-DIRECTORY}\" as a side source file directory"
+elif
+  echo "::debug::Input side directory not provided."  
 fi 
 
-if [ -z "${INPUT_OUTPUT-DIRECTORY}" ]; then
+if [ -n "${INPUT_OUTPUT-DIRECTORY}" ]; then
   echo "::debug::Input output directory provided."
   echo "::debug::Using \"${INPUT_OUTPUT-DIRECTORY}\" as an output directory"
+elif
+  echo "::debug::Input output directory not provided."  
 fi 
 
